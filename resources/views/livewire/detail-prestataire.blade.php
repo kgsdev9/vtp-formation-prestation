11571bafacfc2ca76dc13664252152d1f003e9f3
kgsdev9 <kgsdev8@gmail.com>
2024-10-30 23:01:10 +0100

    my-comited-progress

--- resources/views/livewire/detail-prestataire.blade.php
+++ resources/views/livewire/detail-prestataire.blade.php
@@ -32,15 +32,8 @@
                               </div>
                               <!--button-->
                               <div class="d-flex flex-row gap-3 align-items-center">
-                                <a href="#!" class="btn btn-outline-white">
-                                  <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill me-1" viewBox="0 0 16 16">
-                                      <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"></path>
-                                    </svg>
-                                  </span>
-                                  Ajouter au Favoris
-                                </a>
-                                <a href="#!" class="btn btn-outline-white">Contacter</a>
+
+                                <a href="#!" class="btn btn-outline-white">Me Faire mentorer</a>
                               </div>
                             </div>
                             <div class="d-flex flex-md-row flex-column gap-md-4 gap-2">
@@ -57,19 +50,7 @@
                                   ({{count($prestataire->notePrestations)}}&nbsp;Avis)
                                 </span>
                               </div>
-                              <div class="d-flex flex-row gap-2 align-items-center lh-1">
-                                <!--icon-->
-                                <span>
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-people-fill text-primary align-baseline" viewBox="0 0 16 16">
-                                    <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"></path>
-                                  </svg>
-                                </span>
-                                <!--text-->
-                                <span>
-                                  <span class="text-gray-800 fw-bold">40+</span>
-                                 Likes
-                                </span>
-                              </div>
+                             
                               <div class="d-flex flex-row gap-2 align-items-center lh-1">
                                 <!--icon-->
                                 <span>
